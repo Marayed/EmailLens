8ce1babd5fdb22123c30f9dddf10bdecd0b7cf7c
Benjeddou Houcem <houcembenjeddou@esprit.tn>
2023-07-27 17:18:37 +0100

    El Nino

---
+++ openCV/build/intermediates/cxx/Debug/4p5j46l2/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\benje\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\benje\\Downloads\\EmailLens-main\\EmailLens-main\\openCV\\.cxx\\Debug\\4p5j46l2\\armeabi-v7a" ^
+  opencv_jni_shared
---
+++ openCV/build/intermediates/cxx/Debug/4p5j46l2/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\benje\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\benje\\Downloads\\EmailLens-main\\EmailLens-main\\openCV\\.cxx\\Debug\\4p5j46l2\\x86_64" ^
+  opencv_jni_shared
