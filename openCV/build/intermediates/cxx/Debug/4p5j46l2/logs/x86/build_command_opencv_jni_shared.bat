13a0f672162a7d14a72f3a1629ed0f8c9f0ae454
houcem-benjeddou <houcem.benjeddou@esprit.tn>
2023-08-27 14:08:07 +0100

    Cmaera Fix & Enhancing Quality

--- openCV/build/intermediates/cxx/Debug/4p5j46l2/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\benje\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\benje\\Downloads\\EmailLens-main\\EmailLens-main\\openCV\\.cxx\\Debug\\4p5j46l2\\x86" ^
-  opencv_jni_shared
