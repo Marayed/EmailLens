d6799a08ca36f63f26a4abd4e85713e10c1617c5
Marayed <marayed0@gmail.com>
2023-09-13 12:34:27 +0100

    fixes

--- app/src/main/java/com/example/stage3/Camera.java
+++ app/src/main/java/com/example/stage3/Camera.java
@@ -1,17 +1,36 @@
 package com.example.stage3;
 
-import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Camera extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private CameraBridgeViewBase mOpenCvCameraView;
+    private Button captureButton;
+    private ImageView capturedImageView;
+    private Mat mRgbaFrame;
+    private TessBaseAPI tessBaseAPI;
 
     static {
         if (!OpenCVLoader.initDebug()) {
@@ -28,23 +47,34 @@ public class Camera extends AppCompatActivity implements CameraBridgeViewBase.Cv
         mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
         mOpenCvCameraView.setCameraPermissionGranted();
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        captureButton = findViewById(R.id.capture_button);
+        capturedImageView = findViewById(R.id.captured_image_view);
+
+        // Initialize Tesseract OCR
+        tessBaseAPI = new TessBaseAPI();
+
+        // Copy the trained data from assets to external storage (if not already done)
+        copyTrainedDataToExternalStorage("eng.traineddata");
+
+        // Set the path to the trained data in external storage
+        tessBaseAPI.init(getExternalFilesDir(null).getAbsolutePath(), "eng");
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-        // Camera initialization code
+        mRgbaFrame = new Mat(height, width, CvType.CV_8UC4);
     }
 
     @Override
     public void onCameraViewStopped() {
-        // Camera release code
+        mRgbaFrame.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Mat rgbaFrame = inputFrame.rgba();
-        // Process the frame using OpenCV functions
-        return rgbaFrame;
+        mRgbaFrame = inputFrame.rgba();
+        return mRgbaFrame;
     }
 
     @Override
@@ -67,23 +97,74 @@ public class Camera extends AppCompatActivity implements CameraBridgeViewBase.Cv
         if (mOpenCvCameraView != null) {
             mOpenCvCameraView.disableView();
         }
+
+        // Release Tesseract resources
+        if (tessBaseAPI != null) {
+            tessBaseAPI.clear();
+        }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[],
-                                           int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 1: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mOpenCvCameraView.setCameraPermissionGranted();  // <------ THIS!!!
+    public void captureImage(View view) {
+        if (mRgbaFrame != null) {
+            Bitmap bitmap = Bitmap.createBitmap(mRgbaFrame.cols(), mRgbaFrame.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(mRgbaFrame, bitmap);
+
+            File file = new File(getExternalCacheDir(), "captured_image.png");
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                fos.close();
+
+                // Process the captured image with Tesseract OCR
+                String email = extractEmailFromImage(file.getAbsolutePath());
+
+                if (email != null && !email.isEmpty()) {
+                    // Do something with the extracted email address
+                    Toast.makeText(this, "Email detected: " + email, Toast.LENGTH_SHORT).show();
                 } else {
-                    // permission denied
+                    Toast.makeText(this, "No email address found.", Toast.LENGTH_SHORT).show();
                 }
-                return;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        }
+    }
+
+    private String extractEmailFromImage(String imagePath) {
+        tessBaseAPI.setImage(new File(imagePath));
+        String recognizedText = tessBaseAPI.getUTF8Text();
+        tessBaseAPI.clear();
+
+        // Implement email extraction logic (regex or custom logic) on the recognized text
+        // For simplicity, a basic regex pattern is used here
+        Pattern pattern = Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}");
+        Matcher matcher = pattern.matcher(recognizedText);
+
+        if (matcher.find()) {
+            return matcher.group();
+        }
+
+        return null;
+    }
+
+    private void copyTrainedDataToExternalStorage(String trainedDataName) {
+        AssetManager assetManager = getAssets();
+        String outputPath = new File(getExternalFilesDir(null), trainedDataName).getPath();
+
+        try {
+            InputStream inputStream = assetManager.open("tessdata/" + trainedDataName);
+            OutputStream outputStream = new FileOutputStream(outputPath);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, read);
+            }
+
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- openCV/build/intermediates/cxx/Debug/4p5j46l2/logs/x86_64/build_command_opencv_jni_shared.bat
+++ openCV/build/intermediates/cxx/Debug/4p5j46l2/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
 "C:\\Users\\Marayed\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "C:\\Users\\Marayed\\Desktop\\stage\\EmailLens\\openCV\\.cxx\\Debug\\4p5j46l2\\x86_64" ^
+  "C:\\Users\\Marayed\\Desktop\\rere\\EmailLens\\openCV\\.cxx\\Debug\\4p5j46l2\\x86_64" ^
   opencv_jni_shared
